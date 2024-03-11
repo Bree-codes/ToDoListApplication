cdfdece0b9e44513a06bb300a1e256a0d2c156df
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-03-11 09:42:11 +0300

    initial commit

---
+++ ToDoListApp/src/main/java/com/springboot/todolistapp/ToDoListAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.todolistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListAppApplication.class, args);
+    }
+
+}
