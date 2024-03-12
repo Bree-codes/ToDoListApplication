4cf138a12da1d732842182eb53a773e751b4a6d7
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-03-12 13:37:02 +0300

    updates

--- ToDoListApp/src/main/java/com/springboot/todolistapp/controller/RegistrationController.java
+++ ToDoListApp/src/main/java/com/springboot/todolistapp/controller/RegistrationController.java
@@ -1,8 +1,11 @@
 package com.springboot.todolistapp.controller;
 
+import com.springboot.todolistapp.entity.RegistrationEntity;
+import com.springboot.todolistapp.repository.RegistrationRepository;
 import com.springboot.todolistapp.request.RegistrationRequest;
 import com.springboot.todolistapp.response.RegistrationResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,9 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class RegistrationController {
 
+    @Autowired
+    private RegistrationRepository registrationRepository;
+
     @PostMapping("/register")
     public ResponseEntity<Object> registerUser(@RequestBody RegistrationRequest registrationRequest){
         log.info("Register Request" + registrationRequest);
+
+        RegistrationEntity registration = new RegistrationEntity();
+        registration.getUsername();
+        registration.getEmail();
+        registration.getPassword();
+        registrationRepository.save(registration);
 
         String username = registrationRequest.getUsername();
         String responseMessage = "User " + username + " created successfully";
