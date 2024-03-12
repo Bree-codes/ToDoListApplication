a796e3882f5d2ed69558bdb632b9e7142097eaec
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-03-12 12:10:48 +0300

    updates

---
+++ ToDoListApp/src/main/java/com/springboot/todolistapp/entity/RegistrationEntity.java
@@ -0,0 +1,12 @@
+package com.springboot.todolistapp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "UserRegistrationDetails")
+public class RegistrationEntity {
+
+}
