68923781145355ebc166a3525195581bf558df7f
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-03-12 13:18:20 +0300

    updates

--- ToDoListApp/src/main/java/com/springboot/todolistapp/request/RegistrationRequest.java
+++ ToDoListApp/src/main/java/com/springboot/todolistapp/request/RegistrationRequest.java
@@ -1,8 +1,19 @@
 package com.springboot.todolistapp.request;
 
+import lombok.Data;
+
+@Data
 public class RegistrationRequest {
 
     private String email;
     private String username;
     private String password;
+
+    public RegistrationRequest() {
+    }
+    public RegistrationRequest(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
 }
