1fcb6338c2aaabd3dc183db56aec43d7021a80b6
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-03-12 12:17:01 +0300

    updates

--- ToDoListApp/src/main/java/com/springboot/todolistapp/entity/RegistrationEntity.java
+++ ToDoListApp/src/main/java/com/springboot/todolistapp/entity/RegistrationEntity.java
@@ -1,12 +1,29 @@
 package com.springboot.todolistapp.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
 @Table(name = "UserRegistrationDetails")
 public class RegistrationEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = true)
+    private String username;
+
+    @Column(nullable = true)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String password;
+
 
 }
