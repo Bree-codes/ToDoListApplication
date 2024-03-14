50437f6e7602f2efb7d9d5f095c85ef1951e4c67
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-03-14 12:56:48 +0300

    working on security configurations for login and logout

---
+++ ToDoListApp/src/main/java/com/springboot/todolistapp/configuration/SecurityConfiguration.java
@@ -0,0 +1,10 @@
+package com.springboot.todolistapp.configuration;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.stereotype.Component;
+
+@EnableWebSecurity
+@Component
+public class SecurityConfiguration {
+
+}
