ca41296c6dc3ab830070188bc59540b6badd9d65
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-17 06:38:10 +0300

    working on the frontend

--- todo-list-application/src/main.jsx
+++ todo-list-application/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import  ReactDom from 'react-dom/client'
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDom.createRoot(document.getElementById("root").render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+    )
 )
