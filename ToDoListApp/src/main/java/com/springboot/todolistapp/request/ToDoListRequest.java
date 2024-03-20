baf309c599f314092ff4db5fc1ccfb0a6a38acf6
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-03-20 14:56:49 +0300

    working CRUD operations for the application

--- ToDoListApp/src/main/java/com/springboot/todolistapp/request/ToDoListRequest.java
+++ ToDoListApp/src/main/java/com/springboot/todolistapp/request/ToDoListRequest.java
@@ -1,19 +1,32 @@
 package com.springboot.todolistapp.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Component
 public class ToDoListRequest {
 
     private String activityName;
+
     private Date startTime;
+
     private Date endTime;
 
+    @JsonIgnore
+    private Date date;
+
+    @PostConstruct
+    private void setDate(){
+        this.date = new Date();
+    }
 
 }
