6503843e0014aba3a85275b70854834774faa731
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-27 07:13:33 +0300

    nice footer design

--- todo-list-application/src/Components/SectionComponents/HomeFooter.jsx
+++ todo-list-application/src/Components/SectionComponents/HomeFooter.jsx
@@ -1,9 +1,14 @@
-
+import ack from "./../images/footerLogo.svg"
+import {Container} from "react-bootstrap";
 
 export default function HomeFooter() {
     return (
-        <div style={{height:137}}>
+        <Container style={{height: 137, justifyContent: "center", display: "flex", alignItems: "center"}}>
+            <div style={{height: 137, justifyContent: "center", display: "flex", alignItems: "center"}}>
+                <img src={ack} alt={"footer Logo"}
+                     style={{width: "100%", justifyContent: "center", motionRotation: "initial"}}/>
+            </div>
+        </Container>
 
-        </div>
     );
 }
