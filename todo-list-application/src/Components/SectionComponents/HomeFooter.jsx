fab66f16a6ece84c2384d53d053e8e42c3835149
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-27 08:44:00 +0300

    break

--- todo-list-application/src/Components/SectionComponents/HomeFooter.jsx
+++ todo-list-application/src/Components/SectionComponents/HomeFooter.jsx
@@ -5,19 +5,25 @@ import email from  './../images/emailIcon.svg'
 import {Container} from "react-bootstrap";
 
 export default function HomeFooter() {
+
     return (
         <Container style={{height: "137px", display: "flex", flexDirection: "column", alignItems: "center"}}>
             <div style={{height: "50%", display: "flex", justifyContent: "center", alignItems: "center"}}>
-                <img src={ack} alt={"footer Logo"} style={{width: "auto", height: "100%", maxWidth: "100%", maxHeight: "100%", justifyContent: "center"}}/>
+                <img src={ack} alt={"footer Logo"} style={{width: "auto", height: "100%", maxWidth: "100%",
+                    maxHeight: "100%", justifyContent: "center"}}/>
             </div>
             <div style={{height: "50%", display: "flex", width: "100%"}}>
                 <div style={{flex: 1, display: "flex", justifyContent: "flex-start", alignItems: "center"}}>
-                    <img src={stevo} alt="stevo" style={{width: "10%", height: "30%", margin:"10px"}}/>
-                    <img src={email} alt="email" style={{width: "10%", height: "30%", margin:"10px"}}/>
+                    <img src={stevo} alt="stevo" style={{width: "10%", height: "30%", margin:"10px"}}
+                         onClick={() => window.location.href="https://github.com/Red-stevo"}/>
+                    <img src={email} alt="email" style={{width: "10%", height: "30%", margin:"10px"}}
+                         onClick={() => window.location.href="https://github.com/Red-stevo"}/>
                 </div>
                 <div style={{flex: 1, display: "flex", justifyContent: "flex-end", alignItems: "center"}}>
-                    <img src={bree} alt="bree" style={{width: "10%", height: "30%", margin:"10px"}}/>
-                    <img src={email} alt="email" style={{width: "10%", height: "30%", margin:"10px"}}/>
+                    <img src={bree} alt="bree" style={{width: "10%", height: "30%", margin:"10px"}}
+                    onClick={() => window.location.href="https://github.com/Bree-codes/"}/>
+                    <img src={email} alt="email" style={{width: "10%", height: "30%", margin:"10px"}}
+                         onClick={() => window.location.href="https://github.com/Bree-codes/"}/>
                 </div>
             </div>
         </Container>
