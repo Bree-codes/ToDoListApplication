485260612bbada4ed0d4370828e7827b18d10a4b
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-28 12:13:36 +0300

    done with the design on to the functionality

--- todo-list-application/src/Components/RoutedPages/SingUpPage.jsx
+++ todo-list-application/src/Components/RoutedPages/SingUpPage.jsx
@@ -12,25 +12,25 @@ export default function SingUpPage() {
         <Container className={"container"}>
             <div className="section">
                 <Form onSubmit={handleRegister}>
-                    <Form.Label>Register</Form.Label>
+                    <Form.Label className={"label"}>Register</Form.Label>
 
                    <Form.Group className={"group"}>
-                       <Form.Label className={"star"}>Username</Form.Label>
+                       <Form.Label className={"star label"}>Username</Form.Label>
                        <Form.Control type={"text"} placeholder={"e.g Red-Stevo"} autoComplete={"false"} />
                    </Form.Group>
 
                     <Form.Group className={"group"}>
-                        <Form.Label className={"star"}>Email</Form.Label>
+                        <Form.Label className={"star label"}>Email</Form.Label>
                         <Form.Control type={"email"} placeholder={"breecodes@gmail.com"} autoComplete={"false"}/>
                     </Form.Group>
 
                     <Form.Group className={"group"}>
-                        <Form.Label className={"star"} >Password</Form.Label>
+                        <Form.Label className={"star label"} >Password</Form.Label>
                         <Form.Control type={"password"} autoComplete={"false"}/>
                     </Form.Group>
 
                     <Form.Group className={"group"}>
-                        <Form.Label className={"star"} >Confirm Password</Form.Label>
+                        <Form.Label className={"star label"} >Confirm Password</Form.Label>
                         <Form.Control type={"password"} autoComplete={"false"}/>
                     </Form.Group>
                 </Form>
