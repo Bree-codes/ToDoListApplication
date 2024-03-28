9d7a982126ee9e0095f0ced08a1da6c2cf6da525
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-28 07:37:46 +0300

    added the html for the registration form

--- todo-list-application/src/Components/RoutedPages/SingUpPage.jsx
+++ todo-list-application/src/Components/RoutedPages/SingUpPage.jsx
@@ -1,10 +1,38 @@
 import './../Styles/RotationCss.css'
-import {Container} from "react-bootstrap";
+import {Container, Form} from "react-bootstrap";
+
 export default function SingUpPage() {
+
+
+    const handleRegister = () =>{
+
+    }
+
     return (
         <Container className={"container"}>
             <div className="section">
-                <h1>Hello steve</h1>
+                <Form onSubmit={handleRegister}>
+                    <Form.Label>Register</Form.Label>
+                   <Form.Group>
+                       <Form.Label>Username</Form.Label>
+                       <Form.Control type={"text"} placeholder={"e.g Red-Stevo"} />
+                   </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type={"email"} placeholder={"breecodes@gmail.com"}/>
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type={"password"} />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control type={"password"}/>
+                    </Form.Group>
+                </Form>
             </div>
         </Container>
     );
