ebfdf86750ce7264598cebee41e6a1eac35aacc8
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-28 11:12:43 +0300

    back

--- todo-list-application/src/Components/RoutedPages/SingUpPage.jsx
+++ todo-list-application/src/Components/RoutedPages/SingUpPage.jsx
@@ -13,24 +13,25 @@ export default function SingUpPage() {
             <div className="section">
                 <Form onSubmit={handleRegister}>
                     <Form.Label>Register</Form.Label>
-                   <Form.Group>
+
+                   <Form.Group className={"group"}>
                        <Form.Label>Username</Form.Label>
-                       <Form.Control type={"text"} placeholder={"e.g Red-Stevo"} />
+                       <Form.Control type={"text"} placeholder={"e.g Red-Stevo"} autoComplete={"false"} />
                    </Form.Group>
 
-                    <Form.Group>
+                    <Form.Group className={"group"}>
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type={"email"} placeholder={"breecodes@gmail.com"}/>
+                        <Form.Control type={"email"} placeholder={"breecodes@gmail.com"} autoComplete={"false"}/>
                     </Form.Group>
 
-                    <Form.Group>
+                    <Form.Group className={"group"}>
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type={"password"} />
+                        <Form.Control type={"password"} autoComplete={"false"}/>
                     </Form.Group>
 
-                    <Form.Group>
+                    <Form.Group className={"group"}>
                         <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control type={"password"}/>
+                        <Form.Control type={"password"} autoComplete={"false"}/>
                     </Form.Group>
                 </Form>
             </div>
