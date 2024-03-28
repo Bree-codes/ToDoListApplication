93cc34fe626c4e7b86d732bdb33d7b429d28ca62
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-28 12:34:57 +0300

    configure free api axios

---
+++ todo-list-application/src/Components/BackendSources.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const openAPI = axios.create({
+    baseURL:"https://localhost:8080/api/v1",
+    headers:{
+        Authorization:""
+    }
+})
--- todo-list-application/src/Components/RoutedPages/SingUpPage.jsx
+++ todo-list-application/src/Components/RoutedPages/SingUpPage.jsx
@@ -1,10 +1,19 @@
 import './../Styles/RotationCss.css'
-import {Container, Form} from "react-bootstrap";
+import {Button, Container, Form} from "react-bootstrap";
+import {useState} from "react";
 
 export default function SingUpPage() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
 
     const handleRegister = () =>{
+
+    }
+
+    const handlePasswords = () => {
 
     }
 
@@ -16,23 +25,28 @@ export default function SingUpPage() {
 
                    <Form.Group className={"group"}>
                        <Form.Label className={"star label"}>Username</Form.Label>
-                       <Form.Control type={"text"} placeholder={"e.g Red-Stevo"} autoComplete={"false"} />
+                       <Form.Control type={"text"} placeholder={"e.g Red-Stevo"} autoComplete={"false"}
+                       onChange={(e) => setUsername(e.target.value)}/>
                    </Form.Group>
 
                     <Form.Group className={"group"}>
                         <Form.Label className={"star label"}>Email</Form.Label>
-                        <Form.Control type={"email"} placeholder={"breecodes@gmail.com"} autoComplete={"false"}/>
+                        <Form.Control type={"email"} placeholder={"breecodes@gmail.com"} autoComplete={"false"}
+                        onChange={(e) => setEmail(e.target.value)}/>
                     </Form.Group>
 
                     <Form.Group className={"group"}>
                         <Form.Label className={"star label"} >Password</Form.Label>
-                        <Form.Control type={"password"} autoComplete={"false"}/>
+                        <Form.Control type={"password"} autoComplete={"false"}
+                        onChange={handlePasswords}/>
                     </Form.Group>
 
                     <Form.Group className={"group"}>
                         <Form.Label className={"star label"} >Confirm Password</Form.Label>
-                        <Form.Control type={"password"} autoComplete={"false"}/>
+                        <Form.Control type={"password"} autoComplete={"false"}
+                          onChange={handlePasswords}/>
                     </Form.Group>
+                        <Button type={"submit"}>Register</Button>
                 </Form>
             </div>
         </Container>
