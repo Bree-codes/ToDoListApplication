30da168501f60f6e292c4950206d370c9163e42b
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-04 12:09:22 +0300

    almost there

--- todo-list-application/src/Components/SectionComponents/TimeInput.jsx
+++ todo-list-application/src/Components/SectionComponents/TimeInput.jsx
@@ -1,7 +1,7 @@
 import './../Styles/main.css'
 import {useState} from "react";
 
-s
+
 // eslint-disable-next-line react/prop-types
 export const TimeInput = ({onChange}) =>{
 
@@ -9,24 +9,27 @@ export const TimeInput = ({onChange}) =>{
     const [minutes, setMinutes] = useState();
 
     const handleHours = (e) => {
-
+        setHours(e.target.value);
+        onChange(hours+":"+minutes+":00");
     }
 
     const handleMinutes = (e) => {
-
+        setMinutes(e.target.value);
+        onChange(hours+":"+minutes+":00");
     }
 
     return (
         <>
             <select id="hour" className={'hours'}
-            onChange={e => onChange}>
+            onChange={handleHours} value={hours}>
                 {Array.from({length: 24}, (_, i) => (
                     <option key={i} value={i} className={"bg-dark"}>
                         {i}
                     </option>
                 ))}
             </select>
-            <select id="minute" className={'minutes'} >
+            <select id="minute" className={'minutes'}
+            onChange={handleMinutes} value={minutes}>
                 {Array.from({length: 60}, (_, i) => (
                     <option key={i} value={i} className={"bg-dark"}>
                         {i}
