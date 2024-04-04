ba464eed90cb8150b955932c3409c80921d4842e
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-04 10:45:46 +0300

    changing the inmemory data management

--- todo-list-application/src/Components/SectionComponents/ActivityField.jsx
+++ todo-list-application/src/Components/SectionComponents/ActivityField.jsx
@@ -1,16 +1,9 @@
 import './../Styles/main.css'
 import {Col, Form, Row} from "react-bootstrap";
 import {TimeInput} from "./TimeInput.jsx";
-import {useState} from "react";
-
 
 
 export const ActivityField = () =>{
-    const [activityName, setActivityName] = useState();
-    const [startTime, setStartTime] = useState({hours:0, minutes:0});
-    const [endTime, setEndtTime] = useState({hours:0, minutes:0});
-
-
 
 
 
@@ -20,15 +13,16 @@ export const ActivityField = () =>{
                 <Row>
                     <Col lg className={'m-2 '}>
                         <label htmlFor="activity" className={'label'}>Activity: </label>
-                        <Form.Control id='activity' placeholder="activity-name" className={'activity-name'}/>
+                        <Form.Control id='activity' placeholder="activity-name" className={'activity-name'}
+                         />
                     </Col>
                     <Col lg className={'m-3 '}>
                         <label htmlFor="start" className={'label'}>Start: </label> <br/>
-                        <TimeInput id={'start'}/>
+                        <TimeInput id={'start'} />
                     </Col>
                     <Col lg className={'m-3'}>
                         <label htmlFor="end" className={'label'}>End: </label> <br/>
-                        <TimeInput id={"end"}/>
+                        <TimeInput id={"end"}  />
                     </Col>
                 </Row>
             </Form>
--- todo-list-application/src/Components/SectionComponents/TimeInput.jsx
+++ todo-list-application/src/Components/SectionComponents/TimeInput.jsx
@@ -1,18 +1,19 @@
 import './../Styles/main.css'
 
+// eslint-disable-next-line no-unused-vars,react/prop-types
 export const TimeInput = () =>{
 
 
     return (
         <>
-            <select id="hour" className={'hours'}>
+            <select id="hour" className={'hours'} >
                 {Array.from({length: 24}, (_, i) => (
                     <option key={i} value={i} className={"bg-dark"}>
                         {i}
                     </option>
                 ))}
             </select>
-            <select id="minute" className={'minutes'}>
+            <select id="minute" className={'minutes'} >
                 {Array.from({length: 60}, (_, i) => (
                     <option key={i} value={i} className={"bg-dark"}>
                         {i}
