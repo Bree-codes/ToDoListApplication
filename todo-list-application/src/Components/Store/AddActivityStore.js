7e8949007faf22e52d3598405a89d5f549ad3206
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-04 10:59:11 +0300

    added the store, and the action

---
+++ todo-list-application/src/Components/Store/AddActivityStore.js
@@ -0,0 +1,15 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+export const AddActivityStore = createSlice({
+    name:"todoActivity",
+    initialState:{
+        activity:'',
+        startTime:'00:00:00',
+        endTime:'00:00:00'
+    },
+    reducers:{
+        setActivityName:(state, value) => {state.activity = value.payload},
+        setStartTime:(state, value) => {state.startTime = value.payload},
+        setEndTime:(state, value) => {state.endTime = value.payload}
+    }
+})
