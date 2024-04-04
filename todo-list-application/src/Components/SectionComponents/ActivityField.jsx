185c6f9eeb49822b86c6d2182f88de05cceb98b0
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-04 15:53:41 +0300

    little animation here and there

--- todo-list-application/src/Components/SectionComponents/ActivityField.jsx
+++ todo-list-application/src/Components/SectionComponents/ActivityField.jsx
@@ -1,10 +1,11 @@
-import { Col, Form, Row } from "react-bootstrap";
+import {Col, Form, Row} from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import {setActivityName, setEndTime, setStartTime} from "../Store/AddActivityStore.js";
 import {TimeInput} from "./TimeInput.jsx";
+import './../Styles/main.css'
 
 export const ActivityField = () => {
-    const activity = useSelector(state => state.activity);
+    const activity = useSelector(state => state.activity.activityName);
     const dispatch = useDispatch();
 
 
@@ -16,8 +17,7 @@ export const ActivityField = () => {
                         <label htmlFor="activity" className={'label'}>Activity: </label>
                         <Form.Control id='activity' placeholder="activity-name" className={'activity-name'}
                           onChange={e => dispatch(setActivityName(e.target.value)) }
-                                      onClick={() => console.log(activity)}
-                        />
+                        value={activity}/>
                     </Col>
                     <Col lg className={'m-3 '}>
                         <label htmlFor="start" className={'label'}>Start: </label> <br/>
@@ -27,6 +27,10 @@ export const ActivityField = () => {
                         <label htmlFor="end" className={'label'}>End: </label> <br/>
                         <TimeInput id={"end"} onChange={(value) => dispatch(setEndTime(value))} />
                     </Col>
+                </Row>
+                <Row className={'buttons'}>
+                    <Col xs={2} className={'m-3 column'} ><button className={'add'}>Add</button></Col>
+                    <Col xs={2} className={'m-3 column'}><button className={'done'}>Done</button></Col>
                 </Row>
             </Form>
         </div>
