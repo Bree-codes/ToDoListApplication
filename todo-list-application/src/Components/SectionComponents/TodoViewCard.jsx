4650b8a461128124f098b2dcb187db446dd82e59
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-08 13:18:42 +0300

    break

--- todo-list-application/src/Components/SectionComponents/TodoViewCard.jsx
+++ todo-list-application/src/Components/SectionComponents/TodoViewCard.jsx
@@ -1,11 +1,11 @@
 import {Button, Card} from "react-bootstrap";
+import './../Styles/viewPage.css'
 
 // eslint-disable-next-line react/prop-types
 export const TodoViewCard = ({ date }) => {
     return(
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" alt={"todo list card image"} />
-            <Card.Body>
+        <Card style={{ width: '16rem', background:"transparent", border:"none", color:"white" }}>
+            <Card.Body className={'date'}>
                 <Card.Title>{date}</Card.Title>
                 <Card.Text>
                     Some quick example text to build on the card title and make up the
