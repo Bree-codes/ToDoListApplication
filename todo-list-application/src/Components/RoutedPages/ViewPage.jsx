12c9683969ee38e0468bac2ca5bd903d4561f14e
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-08 10:52:05 +0300

    handle the axios dates fetching

--- todo-list-application/src/Components/RoutedPages/ViewPage.jsx
+++ todo-list-application/src/Components/RoutedPages/ViewPage.jsx
@@ -1,4 +1,27 @@
+import {useEffect, useState} from "react";
+import {getDate} from "../BackendSources.js";
+import {setDates} from "../Store/TodoDates.js";
+import {useDispatch} from "react-redux";
+
 export const  ViewPage = () => {
+    const userId = localStorage.getItem('id');
+    const dispatch = useDispatch();
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        getDate(userId).then((res) =>{
+
+            dispatch(setDates(res.data));
+
+        }).catch(error => {
+
+            setError(error.response.message);
+        });
+
+    }, []);
+
+
+
     return(
         <div>
             <h1>view page</h1>
