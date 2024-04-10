8eef8535dc086ef053cf38509e67a8eda74694ac
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-10 20:03:31 +0300

    configured the refresh token geneartion and setup

--- ToDoListApp/src/main/java/com/springboot/todolistapp/repository/RefreshTokenRepository.java
+++ ToDoListApp/src/main/java/com/springboot/todolistapp/repository/RefreshTokenRepository.java
@@ -1,9 +1,13 @@
 package com.springboot.todolistapp.repository;
 
 import com.springboot.todolistapp.entity.RefreshTokenTable;
+import com.springboot.todolistapp.entity.User;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface RefreshTokenRepository extends CrudRepository<RefreshTokenTable, Long> {
 
-
+    Optional<List<RefreshTokenTable>> findByUser(User user);
 }
--- ToDoListApp/src/main/java/com/springboot/todolistapp/service/RefreshTokenService.java
+++ ToDoListApp/src/main/java/com/springboot/todolistapp/service/RefreshTokenService.java
@@ -1,5 +1,6 @@
 package com.springboot.todolistapp.service;
 
+import com.springboot.todolistapp.entity.RefreshTokenTable;
 import com.springboot.todolistapp.entity.User;
 import com.springboot.todolistapp.repository.RefreshTokenRepository;
 import lombok.Data;
@@ -7,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.UUID;
 
 @Service
@@ -21,13 +23,26 @@ public class RefreshTokenService {
 
         String refreshToken = UUID.randomUUID().toString();
 
-        return UUID.randomUUID().toString();
+        //performing the database operation for refresh token generation.
+        saveRefreshToken(user, refreshToken);
+
+        return refreshToken;
     }
 
     private void saveRefreshToken(User user,String refreshToken){
 
-        refreshTokenRepository.findById()
-    }
+        //get the user refresh tokens if present and delete them before setting a new one
+        refreshTokenRepository.findByUser(user).ifPresent(refreshTokenRepository::deleteAll);
 
+        //saving the refresh token
+
+        RefreshTokenTable refreshTokenTable = new RefreshTokenTable();
+        refreshTokenTable.setRefreshToken(refreshToken);
+        refreshTokenTable.setUser(user);
+        refreshTokenTable.setExpirationDate((new Date(System.currentTimeMillis()+ 1000*60*60*24*14)) );
+
+        //saving to database.
+        refreshTokenRepository.save(refreshTokenTable);
+    }
 
 }
