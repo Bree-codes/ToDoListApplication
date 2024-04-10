854ae12937a0cfac8e7ebae03888fa36e1547018
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-10 19:53:51 +0300

    configuring refresh token saving

--- ToDoListApp/src/main/java/com/springboot/todolistapp/service/RefreshTokenService.java
+++ ToDoListApp/src/main/java/com/springboot/todolistapp/service/RefreshTokenService.java
@@ -1,5 +1,7 @@
 package com.springboot.todolistapp.service;
 
+import com.springboot.todolistapp.entity.User;
+import com.springboot.todolistapp.repository.RefreshTokenRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,8 +15,18 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class RefreshTokenService {
 
-    public String generateRefreshToken(){
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public String generateRefreshToken(User user){
+
+        String refreshToken = UUID.randomUUID().toString();
+
         return UUID.randomUUID().toString();
+    }
+
+    private void saveRefreshToken(User user,String refreshToken){
+
+        refreshTokenRepository.findById()
     }
 
 
