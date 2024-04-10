42bace7a0ca89f68c568380de58772a44b5f434e
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-10 16:22:50 +0300

    removed the csrf disbled

--- ToDoListApp/src/main/java/com/springboot/todolistapp/service/RefreshTokenService.java
+++ ToDoListApp/src/main/java/com/springboot/todolistapp/service/RefreshTokenService.java
@@ -1,4 +1,13 @@
 package com.springboot.todolistapp.service;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@Data
+@RequiredArgsConstructor
 public class RefreshTokenService {
 }
