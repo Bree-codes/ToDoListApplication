dd0d9e76297443119194a3c3eb9e6bc48d26ebbc
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-15 13:12:07 +0300

    designed the main page

--- todo-list-application/src/Components/RoutedPages/CreatePage.jsx
+++ todo-list-application/src/Components/RoutedPages/CreatePage.jsx
@@ -8,7 +8,11 @@ import profileImage from './../images/profileImage.jpeg'
 const CreatePage = () => {
     const navigate = useNavigate();
     const [section, setSection] = useState([]);
+    const [showSidebar, setShowSidebar] = useState(false);
 
+    const toggleSidebar = () => {
+        setShowSidebar(!showSidebar);
+    };
 
     useEffect(() => {
             if(!Auth()){
@@ -40,30 +44,36 @@ const CreatePage = () => {
 
 
     return (
-        <>
+        <div className="container">
             <Row className={"justify-content-center align-items-center"}>
-                <Col className={"col-lg-4 side-bar"}>
-                        <div className={"side-bar-header"}>
-                            <div>
-                                <Image src={profileImage} height={50} width={50} className={"side-bar-image"} />
-                                <br />
-                                <span>{sessionStorage.getItem("username")}</span>
+                <Col className={`col-lg-4 side-bar ${showSidebar ? 'show' : ''}`}>
+                    <div className="side-bar-header">
+                        <div>
+                            <Image src={profileImage} height={50} width={50} className="side-bar-image"/>
+                            <br/>
+                            <span>{sessionStorage.getItem("username")}</span>
                             </div>
-                        </div>
+                        <span className="close-btn" onClick={toggleSidebar}>&times;</span>
+                    </div>
+                    {/* Sidebar content here */}
                 </Col>
-                <Col className={"col-12 col-md-8 input-section"}>
-                    <Row className={"input-header"}>
-                        <Col className={"col-lg-4"} >ACTIVITY</Col>
-                        <Col className={"col-lg-3"}>START </Col>
-                        <Col className={"col-lg-3"}>STOP</Col>
-                        <Col className={"col-lg-1"}>ACTION</Col>
+                <Col className="col-12 col-md-8 input-section">
+                    <Row className="input-header">
+                        <Col className="col-lg-4">ACTIVITY</Col>
+                        <Col className="col-lg-3">START</Col>
+                        <Col className="col-lg-3">STOP</Col>
+                        <Col className="col-lg-1">ACTION</Col>
                     </Row>
-                {section.map((sec) => {
-                    return(sec);
-                })}
+                    {/* Main content here */}
+                    {/* Use enough content to enable scrolling */}
+                    <div>
+                        {section.map((sec) => {
+                            return(sec);
+                        })}
+                    </div>
                 </Col>
             </Row>
-        </>
+        </div>
     );
 };
 
